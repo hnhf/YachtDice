d28ebe46eb551a9b70bef01d4b3c2fe4fb780a0c
hnhf <fanyh2019@163.com>
2021-07-02 01:09:11 +0800

    计算分数函数

--- yachtDice.py
+++ yachtDice.py
@@ -13,7 +13,9 @@ from time import ctime
 import json
 
 # 界面初始化
-screen = pygame.display.set_mode((600, 820))
+x_length = 600
+y_length = 820
+screen = pygame.display.set_mode((x_length, y_length))
 pygame.display.set_caption('游艇骰子')
 bg_color = (255, 255, 255)
 pygame.init()
@@ -29,13 +31,16 @@ red = (255, 0, 0)
 black = (0, 0, 0)
 
 # 分数记录
+player = 1
+player_A_location = 250
+player_B_location = 475
 dice = [1, 2, 3, 4, 5]
 score_now_1 = np.zeros(17, dtype=int)
-score_now_2 = np.zeros(17,dtype=int)
-score_record_1 = np.zeros(17,dtype=int)
-score_record_2 = np.zeros(17,dtype=int)
+score_now_2 = np.zeros(17, dtype=int)
+score_record_1 = np.zeros(17, dtype=int)
+score_record_2 = np.zeros(17, dtype=int)
 list_1 = ['1点', '2点', '3点', '4点', '5点', '6点', 'Bonus', '上半区总分', '三条', '四条',
-          '葫芦', '小顺','大顺', '游艇', '全计', '下半区总分', '总分']
+          '葫芦', '小顺', '大顺', '游艇', '全计', '下半区总分', '总分']
 
 # 显示数字
 font_color = (0, 0, 0)
@@ -54,36 +59,91 @@ def draw_board():
     player_A = font_middle.render('A', True, font_color)
     player_B = font_middle.render('B', True, font_color)
     screen.blit(players, (35, 110))
-    screen.blit(player_A, (248, 110))
-    screen.blit(player_B, (473, 110))
+    screen.blit(player_A, (player_A_location-2, 110))
+    screen.blit(player_B, (player_B_location-2, 110))
     for j in range(17):
         element_1 = font_small.render(list_1[j], True, font_color)
         screen.blit(element_1, (50, 160 + j * 40))
         single_score_1 = font_small.render(str(score_record_1[j]), True, font_color)
         single_score_2 = font_small.render(str(score_record_2[j]), True, font_color)
-        screen.blit(single_score_1, (250, 160 + j * 40))
-        screen.blit(single_score_2, (475, 160 + j * 40))
-    pygame.draw.line(screen, black, (0, 100), (600, 100), (3))
-    pygame.draw.line(screen, black, (0, 100), (600, 100), (3))
-    pygame.draw.line(screen, black, (0, 150), (600, 150), (3))
-    pygame.draw.line(screen, gray, (0, 430), (600, 430), (3))
-    pygame.draw.line(screen, gray, (0, 470), (600, 470), (3))
-    pygame.draw.line(screen, gray, (0, 750), (600, 750), (3))
-    pygame.draw.line(screen, black, (0, 790), (600, 790), (3))
-    pygame.draw.line(screen, gray, (150, 100), (150, 820), (3))
-    pygame.draw.line(screen, gray, (375, 100), (375, 820), (3))
+        screen.blit(single_score_1, (player_A_location, 160 + j * 40))
+        screen.blit(single_score_2, (player_B_location, 160 + j * 40))
+    pygame.draw.line(screen, black, (0, 100), (x_length, 100), (3))
+    pygame.draw.line(screen, black, (0, 100), (x_length, 100), (3))
+    pygame.draw.line(screen, black, (0, 150), (x_length, 150), (3))
+    pygame.draw.line(screen, gray, (0, 430), (x_length, 430), (3))
+    pygame.draw.line(screen, gray, (0, 470), (x_length, 470), (3))
+    pygame.draw.line(screen, gray, (0, 750), (x_length, 750), (3))
+    pygame.draw.line(screen, black, (0, 790), (x_length, 790), (3))
+    pygame.draw.line(screen, gray, (150, 100), (150, y_length), (3))
+    pygame.draw.line(screen, gray, (375, 100), (375, y_length), (3))
     pygame.display.update()
+
+
+def draw_again():
+    for i in range(5):
+        screen.blit(img[dice[i]], (i * 100, 0))
+    score_now = count_score()
+    if player == 1:
+        location = player_A_location
+    else:
+        location = player_B_location
+    for j in range(17):
+        if score_record_1[j] != -1:
+            single_score_1 = font_small.render(str(score_now[j]), True, font_color)
+            screen.blit(single_score_1, (location, 160 + j * 40))
+    pygame.display.update()
+
+
+def count_score():
+    score_now = np.zeros(17, dtype=int)
+    dice1 = dice[:]  # 复制骰子数列
+    dice_set = set(dice)  # 骰子点数集合
+    dice_sum = sum(dice)  # 骰子点数之和
+    dice1.sort()  # 骰子点数重排
+    # 1到6单独点数分数
+    for dice_i in range(5):
+        for score_i in range(5):
+            for point_i in range(6):
+                if dice1[dice_i] == point_i + 1:
+                    score_now[score_i] += point_i + 1
+    # 三条
+    if dice1[0] == dice1[2] or dice1[1] == dice1[3] or dice1[2] == dice1[4]:
+        score_now[9] = dice_sum
+    # 四条
+    if dice1[0] == dice1[3] or dice1[1] == dice1[4]:
+        score_now[10] = dice_sum
+    # 葫芦
+    if dice1[0] == dice1[2] and dice1[3] == dice1[4] or \
+            dice1[0] == dice1[1] and dice1[2] == dice1[4]:
+        score_now[11] = 25
+    # 小顺
+    if dice_set & {1, 2, 3, 4} == {1, 2, 3, 4} or \
+            dice_set & {2, 3, 4, 5} == {2, 3, 4, 5} or \
+            dice_set & {3, 4, 5, 6} == {3, 4, 5, 6}:
+        score_now[12] = 30
+    # 大顺
+    if dice_set & {1, 2, 3, 4, 5} == {1, 2, 3, 4, 5} or \
+            dice_set & {2, 3, 4, 5, 6} == {2, 3, 4, 5, 6}:
+        score_now[13] = 40
+    # 游艇
+    if dice1[0] == dice1[4]:
+        score_now[14] = 50
+    # 全计
+    score_now[15] = dice_sum
+    return score_now
 
 while True:
     draw_board()
 
-# #掷骰子
-# def roll_dice():
-#     if event.type == MOUSEBUTTONDOWN:
-#         pos_dice = pygame.mouse.get_pos()
-#         if pos_dice
-#
-#
+#掷骰子
+def roll_dice():
+    if roll_time == 0:
+    if event.type == MOUSEBUTTONDOWN:
+        pos_dice = pygame.mouse.get_pos()
+        if pos_dice
+
+
 # def draw_dice():
 #     for i in range(5):
 #         screen.blit(img[dice[i]],(50, i*50))
