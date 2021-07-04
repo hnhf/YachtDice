bd4b47be6a5f51113149dcbb77c03e95b31cb74f
scx <scx1998@aliyun.com>
2021-07-05 00:29:03 +0800

    sever

--- socket_client.py
+++ socket_client.py
@@ -1,13 +1,6 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 建立连接:
-s.connect(('112.232.240.231', 61746))
-# 接收欢迎消息:
-print(s.recv(1024).decode('utf-8'))
-for data in [b'Michael', b'Tracy', b'Sarah']:
-    # 发送数据:
-    s.send(data)
-    print(s.recv(1024).decode('utf-8'))
-s.send(b'exit')
-s.close()
+s = socket.socket()
+s.connect(('112.232.240.231', 6666))
+s.send("你好呀，我是客户端".encode('utf8'))
+input("")
